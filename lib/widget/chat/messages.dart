da2c442dd78b32c26b2a19630037ecc057a255a1
Bishal KC <kcbisal1@gmail.com>
2021-06-28 21:50:16 +0530

    Add All Files

---
+++ lib/widget/chat/messages.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import './message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var user = FirebaseAuth.instance.currentUser;
+
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapShot) {
+        if (chatSnapShot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapShot.data.docs;
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => MessageBubble(
+            chatDocs[index]['text'],
+            chatDocs[index]['username'],
+            chatDocs[index]['userImage'],
+            chatDocs[index]['userId'] == user.uid,
+            key: ValueKey(chatDocs[index].documentID),
+          ),
+        );
+      },
+    );
+  }
+}
